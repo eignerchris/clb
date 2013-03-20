572d5db4285c236e870e8d57c80ee45c9d15ca54
Chris Eigner <eignerchris@gmail.com>
2013-03-20 06:03:41 -0700

    fully async lb stubbed out

--- src/clb/core.clj
+++ src/clb/core.clj
@@ -1,44 +1,33 @@
 (ns clb.core
   (:require [clj-redis.client :as redis]
-            [org.httpkit.client :as http])
+            [org.httpkit.client :as http]
+            [clojure.string :as str])
   (:use org.httpkit.server))
 
 (defn -main
   "clojure load balancer"
   [& args]
-  (println "Starting clojure load balancer...")
-  (println "Load balancing across:")
-  
-  ; setup server list
-  ; TODO: move to config file
-  (def server-list ["127.0.0.1" "127.0.0.1"])
+  (def server-list ["127.0.0.1:3000" "127.0.0.1:3000"])
   (def port 9090)
   (def formatted-server-list (map (fn [x] (str "   * " x)) server-list))
   
   (dorun (map println formatted-server-list))
 
   (defn async-request-handler [ring-request]
-  (def new-request ring-request)
-  (def random-server (rand-nth server-list))
-  
-  (println 
-    (get ring-request :request-method) 
-    (get ring-request :remote-addr) ">" random-server)
-  
-  (assoc new-request :server-name random-server)
-  (assoc new-request :server-port 80)
+    (async-response respond ; Use this callback when ready
+      (future
+        (def random-server (rand-nth server-list))
+        (def scheme (name (:scheme ring-request)))
+        (def method (:request-method ring-request))
+        (def server-port (:port 3000))
+        (def user-agent (:user-agent ring-request))
+        (http/get (str scheme "://" random-server) {}
+          (fn [{:keys [status headers body error]}] ;; asynchronous handle response
+            (future ; Respond from any thread!
+              (respond {:status status
+                        :headers (dorun (map str headers))
+                        :body    body})))))))
 
-  ; {:remote-addr 127.0.0.1, :scheme :http, :request-method :get, :query-string nil, :content-type nil, :uri /, :server-name 127.0.0.1, :headers {accept */*, host 127.0.0.1:9090, user-agent curl/7.24.0 (x86_64-apple-darwin12.0) libcurl/7.24.0 OpenSSL/0.9.8r zlib/1.2.5}, :content-length 0, :server-port 9090, :character-encoding utf8, :body nil}
-  (def http-method-call (load-string "http/get"))
 
-  (http-method-call "http://www.google.com")
-
-  ; send response to async-response method below
-  (async-response respond ; Use this callback when ready
-    (future ; Respond from any thread!
-      (respond {:status 200
-                :headers {"Content-Type" "text/plain"}
-                :body    "This response was delivered asynchronously!"}))))
-
-  (run-server async-request-handler {:port port}) ; Ring server
+  (run-server async-request-handler {:port port})
 )
